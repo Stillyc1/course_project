dab0d3aa14a8f335caa2179b29630bb9ff84f517
Alexander_Baranov <Sti_ll@mail.ru>
2024-07-18 13:55:03 +0300

    add func in ulitls.py

---
+++ src/utils.py
@@ -0,0 +1,49 @@
+import datetime
+import pandas as pd
+from collections import Counter
+
+
+PATH_FILE = "data/operations.xlsx"
+test_file = "data/test.xlsx"
+
+
+def opening_file(path_file: str) -> pd.DataFrame:
+    operations_xlsx = pd.read_excel(path_file)
+    return operations_xlsx
+
+
+def greeting() -> str:
+    """ возвращает приветствие, в зависимости от времени"""
+
+    time = datetime.datetime.now().hour
+    if time in range(0, 7):
+        return f"Доброй ночи"
+    elif time in range(6, 13):
+        return f"Доброе утро"
+    if time in range(12, 19):
+        return f"Добрый день"
+    else:
+        return f"Добрый вечер"
+
+
+def get_info_cards(operations_xlsx: pd.DataFrame) -> list[dict]:
+    group_card = operations_xlsx.groupby("Номер карты", as_index=False)
+
+    total_sum_cashback = group_card.sum().loc[:, ["Номер карты", "Сумма платежа", "Кэшбэк"]]
+
+    return total_sum_cashback.to_dict(orient="records")
+
+
+def top_transactions(operations_xlsx: pd.DataFrame) -> list[dict]:
+
+    top_5_operation = operations_xlsx.sort_values(by="Сумма платежа").head()
+    information = top_5_operation.loc[:, ["Дата платежа", "Сумма платежа", "Категория", "Описание"]]
+
+    return information.to_dict(orient="records")
+
+
+if __name__ == "__main__":
+    file = opening_file("../data/test.xlsx")
+    x = opening_file("../data/operations.xlsx")
+    print(get_info_cards(x))
+    print(top_transactions(x))
--- src/views.py
+++ src/views.py
@@ -1,2 +1,14 @@
-def func():
-    return 1
+from src.utils import greeting, opening_file, get_info_cards, top_transactions
+
+
+def views(data: str) -> dict:
+    informations_user = dict()
+
+    informations_user["greeting"] = greeting()
+    informations_user["cards"] = get_info_cards("../data/operations.xlsx")
+
+    return informations_user
+
+
+if __name__ == "__main__":
+    print(views("22"))
